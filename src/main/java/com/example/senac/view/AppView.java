7ad49128e31522f511e89baf85b1c2d12d14f919
Milena Bregalda <milenabregaldasenac@gmail.com>
2024-06-01 22:47:30 -0300

    Volta a começar com LoginView

--- src/main/java/com/example/senac/view/AppView.java
+++ src/main/java/com/example/senac/view/AppView.java
@@ -47,7 +47,7 @@ public class AppView extends JPanel {
         painel.add(confirmacaoPedidoView, "confirmacaoPedido");
 
         add(painel);
-        cardLayout.show(painel, "cyberStation");
+        cardLayout.show(painel, "login");
         // ATENÇÃO: Trocar o nome "login" pela janela em que você está mexendo para mostrá-la direto na execução enquanto desenvolve,
         // mas a versão o final começa com login
         // Esse é o método que é chamado para trocar o conteúdo da janela, também colocá-lo nos JButtons necessários
