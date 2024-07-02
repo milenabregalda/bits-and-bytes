92c81135bdf45db60910955e17ac717335b49ada
Milena Bregalda <milenabregaldasenac@gmail.com>
2024-07-02 10:15:17 -0300

    Modifica versão de JUnit

--- src/test/java/com/example/senac/AppTest.java
+++ src/test/java/com/example/senac/AppTest.java
@@ -1,8 +1,9 @@
 package com.example.senac;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.Test;
+import org.junit.jupiter.*;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
